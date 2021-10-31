514b407dbe1b6d20aca00ee53c41a48f70c5157f
shakilmia8 <shakilmia8@gmail.com>
2021-10-31 12:17:09 +0600

    twelveth commit

--- src/components/Update/Update.js
+++ src/components/Update/Update.js
@@ -12,11 +12,65 @@ const Update = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setService(data))
-    }, [])
+    }, []);
+
+    // update User
+    const handleNameChange = e => {
+        const updatedName = e.target.value;
+        const updatedService = { ...service };
+        updatedService.name = updatedName;
+        setService(updatedService);
+    }
+
+    const handleCountryChange = e => {
+        const updatedCountry = e.target.value;
+        const updatedService = { ...service };
+        updatedService.country = updatedCountry;
+        setService(updatedService);
+    }
+
+    const handleImgUrlChange = e => {
+        const updatedImg = e.target.value;
+        const updatedService = { ...service };
+        updatedService.img = updatedImg;
+        setService(updatedService);
+    }
+
+    const handleDescribeChange = e => {
+        const updatedDescribe = e.target.value;
+        const updatedService = { ...service };
+        updatedService.describe = updatedDescribe;
+        setService(updatedService);
+    }
+
+    const handleUpdateService = e => {
+        const url = `http://localhost:9000/services/${id}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(service)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    alert('Updated Successfully');
+                    setService({});
+                }
+            })
+        e.preventDefault();
+    }
     return (
-        <div>
+        <div className='add-service'>
             <h2>Update {service.name} </h2>
-            <p>{id}</p>
+            <form onSubmit={handleUpdateService}>
+                <input onChange={handleNameChange} value={service.name || ''} type="text" /><br />
+                <input onChange={handleCountryChange} value={service.country || ''} type="text" /><br />
+                <input onChange={handleImgUrlChange} value={service.img || ''} type="text" /><br />
+                <textarea onChange={handleDescribeChange} value={service.describe || ''} name="" id="" cols="30" rows="10"></textarea><br />
+                <input type="submit" value="Updated this service" />
+            </form>
         </div>
     );
 };
